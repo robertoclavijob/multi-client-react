73b8dd0cb686cd0a23f4298c2ca347825cf6022f
Roberto Clavijo <claviro85@gmail.com>
2024-11-06 17:21:45 -0400

    Move configurations to configuration.js

---
+++ src/config/configuration.js
@@ -0,0 +1,11 @@
+const configuration = {
+    clientName: "McDonald's",
+    apiKey: "1234567890abcdef",
+    colors: {
+      primary: "#ffcc00",
+      secondary: "#d32f2f",
+    },
+    contactPhone: "1-800-MCD-HELP",
+  };
+  
+  export default configuration;
--- src/config/messages.js
+++ src/config/messages.js
@@ -1,21 +1,22 @@
+import configuration from './configuration';
+
 const messages = {
-    app: {
-      welcome: "Welcome to McDonald's",
-    },
-    promotion: {
-      title: "Special Promotion",
-      text: "Enjoy a delicious Big Mac today!",
-      apiKey: "API Key: 1234567890abcdef"
-    },
-    contact: {
-      title: "Contact Us",
-      text: "Call us at 1-800-MCD-HELP for support."
-    },
-    special: {
-      title: "Today's Special",
-      text: "Get 2 for $5 on Chicken Nuggets!"
-    }
-  };
-  
-  export default messages;
-  
+  app: {
+    welcome: `Welcome to ${configuration.clientName}`,
+  },
+  promotion: {
+    title: "Special Promotion",
+    text: `Enjoy a delicious Big Mac today!`,
+    apiKey: `API Key: ${configuration.apiKey}`
+  },
+  contact: {
+    title: "Contact Us",
+    text: `Call us at ${configuration.contactPhone} for support.`
+  },
+  special: {
+    title: "Today's Special",
+    text: "Get 2 for $5 on Chicken Nuggets!"
+  }
+};
+
+export default messages;
